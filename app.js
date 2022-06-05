9241da3ef69ac35012bd91a22d742fd53da39f0e
Marcelo Daros <marcelo.sdaros@gmail.com>
2022-06-05 16:09:20 -0300

    final version

--- app.js
+++ app.js
@@ -6,50 +6,65 @@ const bodyParser = require("body-parser")
 const posts = db.posts
 const app = express()
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.post("/posts", (req, res) => {
-  const newPost = { id: crypto.randomUUID(), ...req.body}
 
+  const { titulo, conteudo, autor, ano } = req.body
+
+  if (!titulo || !conteudo || !autor || !ano) {
+    return res.status(404).send("Nenhum post foi inserido ainda ou um dos quatro campos obrigatórios está faltando: título, conteudo, autor, ano")
+  }
+
+  const newPost = { id: crypto.randomUUID(), ...req.body}
   posts.push(newPost)
   res.send(newPost)
-  console.log("Post salvo!")
+  return console.log(`Post ${titulo} salvo!`)
 })
 
 app.get("/posts", (req, res) => {
   res.send(posts)
+  return console.log("Posts retornados!")
 })
 
 app.get("/posts/:id", (req, res) => {
   const id = req.params.id
   const index = posts.findIndex((p) => p.id === id)
 
-  if (!index) {
-    return res.status(404).send("Usuário não encontrado")
+  if (index === -1) {
+    return res.status(404).send("Post não encontrado")
   }
+  res.send(posts[index])
+  return console.log("Post retornado!")
 })
 
-app.put("/posts/:id", (req, res) => {
+app.patch("/posts/:id", (req, res) => {
   const id = req.params.id
   const index = posts.findIndex((p) => p.id === id)
 
   if (!index) {
     return res.status(404).send("Post não encontrado!")
   }
+  if (req.body.id) {
+    return res.status(500).send("O campo id não pode ser alterado, retire-o e tente alterar o post novamente apenas com as informações que podem ser editadas!")
+  }
 
-  posts[index] = req.body
+  posts[index] = {...posts[index], ...req.body}
   res.send(posts[index])
+  return console.log("Post atualizado!")
 })
 
 app.delete("/posts/:id", (req, res) => {
   const id = req.params.id
   const index = posts.findIndex((p) => p.id === id)
 
-  if (!index) {
+  if (index === -1) {
     return res.status(404).send("Post não encontrado!")
   }
 
   posts.splice(index, 1)
-  return res.send(posts)
+  res.send(posts[index])
+  return console.log("Post deletado!")
 })
 
 app.listen(7000, () => {
